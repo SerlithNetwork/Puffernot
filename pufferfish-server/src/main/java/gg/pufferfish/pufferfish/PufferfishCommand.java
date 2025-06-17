3516f39c261eed5ddb2a55c935353554d28060e9
Biquaternions <biquaternions@serlith.net>
2025-06-17 16:08:04 -0500

    Fix colors

--- pufferfish-server/src/main/java/gg/pufferfish/pufferfish/PufferfishCommand.java
+++ pufferfish-server/src/main/java/gg/pufferfish/pufferfish/PufferfishCommand.java
@@ -16,7 +16,7 @@ import java.util.Set;
 public class PufferfishCommand {
 
     private static final Component PREFIX = Component.text("Pufferfish » ", TextColor.fromHexString("#12fff6"), TextDecoration.BOLD);
-    private static final Component FEEDBACK_RELOAD_SUCCESS = PREFIX.append(Component.text("Pufferfish configuration has been reloaded."));
+    private static final Component FEEDBACK_RELOAD_SUCCESS = PREFIX.append(Component.text("Pufferfish configuration has been reloaded.", NamedTextColor.WHITE));
     private static final Component FEEDBACK_RELOAD_FAILED = PREFIX.append(Component.text("Failed to reload.", NamedTextColor.RED));
     private static Component FEEDBACK_CURRENT_VERSION = null;
 
@@ -43,7 +43,7 @@ public class PufferfishCommand {
             .then(Commands.literal("version")
                 .executes(ctx -> {
                     if (FEEDBACK_CURRENT_VERSION == null) {
-                        FEEDBACK_CURRENT_VERSION = PREFIX.append(Component.text("This server is running " + Bukkit.getName() + " version " + Bukkit.getVersion() + " (Implementing API version " + Bukkit.getBukkitVersion() + ")"));
+                        FEEDBACK_CURRENT_VERSION = PREFIX.append(Component.text("This server is running " + Bukkit.getName() + " version " + Bukkit.getVersion() + " (Implementing API version " + Bukkit.getBukkitVersion() + ")", NamedTextColor.WHITE));
                     }
                     ctx.getSource().getSender().sendMessage(FEEDBACK_CURRENT_VERSION);
                     return Command.SINGLE_SUCCESS;
