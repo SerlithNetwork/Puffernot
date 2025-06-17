16aa7dd9c6f6a4be4a7a0098c22646b5324fd040
Biquaternions <biquaternions@serlith.net>
2025-06-17 16:04:30 -0500

    Add prefix to version fetcher

--- pufferfish-server/src/main/java/gg/pufferfish/pufferfish/PufferfishCommand.java
+++ pufferfish-server/src/main/java/gg/pufferfish/pufferfish/PufferfishCommand.java
@@ -18,6 +18,7 @@ public class PufferfishCommand {
     private static final Component PREFIX = Component.text("Pufferfish » ", TextColor.fromHexString("#12fff6"), TextDecoration.BOLD);
     private static final Component FEEDBACK_RELOAD_SUCCESS = PREFIX.append(Component.text("Pufferfish configuration has been reloaded."));
     private static final Component FEEDBACK_RELOAD_FAILED = PREFIX.append(Component.text("Failed to reload.", NamedTextColor.RED));
+    private static Component FEEDBACK_CURRENT_VERSION = null;
 
     public static void init() {
 
@@ -41,7 +42,10 @@ public class PufferfishCommand {
             )
             .then(Commands.literal("version")
                 .executes(ctx -> {
-                    ctx.getSource().getSender().sendMessage(Component.text("This server is running " + Bukkit.getName() + " version " + Bukkit.getVersion() + " (Implementing API version " + Bukkit.getBukkitVersion() + ")"));
+                    if (FEEDBACK_CURRENT_VERSION == null) {
+                        FEEDBACK_CURRENT_VERSION = PREFIX.append(Component.text("This server is running " + Bukkit.getName() + " version " + Bukkit.getVersion() + " (Implementing API version " + Bukkit.getBukkitVersion() + ")"));
+                    }
+                    ctx.getSource().getSender().sendMessage(FEEDBACK_CURRENT_VERSION);
                     return Command.SINGLE_SUCCESS;
                 })
             )
