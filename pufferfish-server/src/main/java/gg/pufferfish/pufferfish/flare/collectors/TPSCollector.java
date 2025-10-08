edd6e3fa1acb51136972b4fce53b35f7b3318871
Biquaternions <biquaternions@serlith.net>
2025-10-07 20:33:08 -0500

    Update tps catchup and collector

--- pufferfish-server/src/main/java/gg/pufferfish/pufferfish/flare/collectors/TPSCollector.java
+++ pufferfish-server/src/main/java/gg/pufferfish/pufferfish/flare/collectors/TPSCollector.java
@@ -1,5 +1,6 @@
 package gg.pufferfish.pufferfish.flare.collectors;
 
+import ca.spottedleaf.moonrise.common.time.TickData;
 import co.technove.flare.live.CollectorData;
 import co.technove.flare.live.LiveCollector;
 import co.technove.flare.live.formatter.SuffixFormatter;
@@ -8,7 +9,6 @@ import net.minecraft.server.MinecraftServer;
 import org.bukkit.Bukkit;
 
 import java.time.Duration;
-import java.util.Arrays;
 
 public class TPSCollector extends LiveCollector {
     private static final CollectorData TPS = new CollectorData("airplane:tps", "TPS", "Ticks per second, or how fast the server updates. For a smooth server this should be a constant 20TPS.", SuffixFormatter.of("TPS"), CustomCategories.MC_PERF);
@@ -22,8 +22,8 @@ public class TPSCollector extends LiveCollector {
 
     @Override
     public void run() {
-        long[] times = MinecraftServer.getServer().tickTimes5s.getTimes();
-        double mspt = ((double) Arrays.stream(times).sum() / (double) times.length) * 1.0E-6D;
+        TickData.TickReportData data = MinecraftServer.getServer().tickTimes5s.generateTickReport(null, System.nanoTime(), MinecraftServer.getServer().tickRateManager().nanosecondsPerTick());
+        double mspt = data == null ? 0.0 : data.timePerTickData().segmentAll().average();
 
         this.report(TPS, Math.min(20D, Math.round(Bukkit.getServer().getTPS()[0] * 100d) / 100d));
         this.report(MSPT, (double) Math.round(mspt * 100d) / 100d);
