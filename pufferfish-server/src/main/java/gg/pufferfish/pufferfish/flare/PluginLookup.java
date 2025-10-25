185280a4f5e4d5e34bbca53e226c4f2be9b68638
Biquaternions <biquaternions@serlith.net>
2025-10-25 16:19:23 -0500

    Fix plugin lookup

--- pufferfish-server/src/main/java/gg/pufferfish/pufferfish/flare/PluginLookup.java
+++ pufferfish-server/src/main/java/gg/pufferfish/pufferfish/flare/PluginLookup.java
@@ -3,47 +3,79 @@ package gg.pufferfish.pufferfish.flare;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import io.papermc.paper.plugin.entrypoint.classloader.PaperPluginClassLoader;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.PluginClassLoader;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class PluginLookup {
-    private static final Cache<String, String> pluginNameCache = CacheBuilder.newBuilder()
+
+    private static final Cache<String, String> PLUGIN_NAME_CACHE = CacheBuilder.newBuilder()
         .expireAfterAccess(1, TimeUnit.MINUTES)
         .maximumSize(1024)
         .build();
+    private static final Cache<Boolean, Collection<ClassLoader>> CLASS_LOADER_CACHE = CacheBuilder.newBuilder() // Yes, boolean key, trust
+        .expireAfterAccess(1, TimeUnit.MINUTES)
+        .build();
 
     public static Optional<String> getPluginForClass(String name) {
-        if (name.startsWith("net.minecraft") || name.startsWith("java.") || name.startsWith("com.mojang") ||
-            name.startsWith("com.google") || name.startsWith("it.unimi") || name.startsWith("sun")) {
+        if (name.endsWith(".so") ||
+            name.startsWith("net.minecraft.") || name.startsWith("java.") || name.startsWith("com.mojang.") ||
+            name.startsWith("com.google.") || name.startsWith("it.unimi.") || name.startsWith("sun.") || name.startsWith("javax.") ||
+            name.startsWith("jdk.") || name.startsWith("io.papermc.") || name.startsWith("gg.pufferfish.") || name.startsWith("co.technove.") ||
+            name.startsWith("ca.spottedleaf.") || name.startsWith("com.sun.") || name.startsWith("org.jline.")
+        ) {
             return Optional.empty();
         }
 
-        String existing = pluginNameCache.getIfPresent(name);
+        String existing = PLUGIN_NAME_CACHE.getIfPresent(name);
         if (existing != null) {
             return Optional.ofNullable(existing.isEmpty() ? null : existing);
         }
 
+        Collection<ClassLoader> classLoaders;
         try {
-            Class<?> clazz = Class.forName(name);
-            ClassLoader loader = clazz.getClassLoader();
-
-            String pluginName = "";
-            if (loader instanceof PluginClassLoader bukkitLoader) {
-                pluginName = bukkitLoader.airplane$getPlugin().getName();
-            } else if (loader instanceof PaperPluginClassLoader paperLoader) {
-                JavaPlugin plugin = paperLoader.airplane$getLoadedJavaPlugin();
-                if (plugin != null) {
-                    pluginName = plugin.getName();
-                }
-            }
-            pluginNameCache.put(name, pluginName);
-            return Optional.ofNullable(pluginName.isEmpty() ? null : pluginName);
-
-        } catch (Exception ignored) {
+            classLoaders = CLASS_LOADER_CACHE.get(true, PluginLookup::loadClassLoaders);
+        } catch (ExecutionException ignore) {
             return Optional.empty();
         }
+
+        @Nullable ClassLoader classLoader = PluginLookup.matchClassLoader(classLoaders, name);
+        if (classLoader == null) {
+            return Optional.empty();
+        }
+
+        String pluginName = "";
+        if (classLoader instanceof PluginClassLoader bukkitLoader) {
+            pluginName = bukkitLoader.airplane$getPlugin().getName();
+        } else if (classLoader instanceof PaperPluginClassLoader paperLoader) {
+            JavaPlugin plugin = paperLoader.airplane$getLoadedJavaPlugin();
+            if (plugin != null) {
+                pluginName = plugin.getName();
+            }
+        }
+        PLUGIN_NAME_CACHE.put(name, pluginName);
+        return Optional.ofNullable(pluginName.isEmpty() ? null : pluginName);
     }
+
+    private static @Nullable ClassLoader matchClassLoader(Collection<ClassLoader> classLoaders, String className) {
+        for (ClassLoader classLoader : classLoaders) {
+            try {
+                classLoader.loadClass(className);
+                return classLoader;
+            } catch (ClassNotFoundException ignore) {}
+        }
+        return null;
+    }
+
+    private static Collection<ClassLoader> loadClassLoaders() {
+        return Arrays.stream(Bukkit.getPluginManager().getPlugins()).map(i -> i.getClass().getClassLoader()).toList();
+    }
+
 }
